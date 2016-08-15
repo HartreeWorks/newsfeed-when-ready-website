b18d9bb9100cf7c4e5e68b97d53b6aafe58360fe
Peter Hartree <web@peterhartree.co.uk>
2016-08-15 19:29:58 +0000

    Add inline install
    Resolves #1
    
    Signed-off-by: Peter Hartree <web@peterhartree.co.uk>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,31 @@
-console.log('\'Allo \'Allo!');
+var installButtons = document.querySelectorAll('.btn-install');
+var installButtonLabels = document.querySelectorAll('.wrap-text-install');
+
+var webstoreUrl = 'https://chrome.google.com/webstore/detail/cdedhgmbfjhobfnphaoihdfmnjidcpim';
+
+installButtons.forEach(function(installButton) {
+  installButton.addEventListener('click', function(e) {
+
+    if(chrome && chrome.webstore) {
+      e.preventDefault();
+
+      installButtonLabels.forEach(function(installButtonLabel) {
+        installButtonLabel.innerText = 'Installing...'
+      });
+
+      chrome.webstore.install(webstoreUrl, function() {
+        // Inline install succeeded
+        installButtonLabels.forEach(function(installButtonLabel) {
+          installButtonLabel.innerText = 'Install complete'
+        });
+      },
+      function() {
+        // Inline install failed
+        installButtonLabels.forEach(function(installButtonLabel) {
+          installButtonLabel.innerText = 'Redirecting...'
+        });
+        window.location.href = webstoreUrl;
+      });
+    }
+  });
+})
