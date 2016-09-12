00586716f808e432345dbad9f5fc64fdbbc29d6a
Peter Hartree <web@peterhartree.co.uk>
2016-09-12 09:22:40 +0000

    Send analytics events when install button is clicked and when install succeeds or fails
    Resolves #20
    
    Signed-off-by: Peter Hartree <web@peterhartree.co.uk>

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,26 +6,35 @@ var webstoreUrl = 'https://chrome.google.com/webstore/detail/cdedhgmbfjhobfnphao
 installButtons.forEach(function(installButton) {
   installButton.addEventListener('click', function(e) {
 
+    ga('send', 'event', 'Inline install', 'Started');
+
     if(chrome && chrome.webstore) {
       e.preventDefault();
 
       installButtonLabels.forEach(function(installButtonLabel) {
-        installButtonLabel.innerText = 'Installing...'
+        installButtonLabel.innerText = 'Installing...';
       });
 
       chrome.webstore.install(webstoreUrl, function() {
         // Inline install succeeded
         installButtonLabels.forEach(function(installButtonLabel) {
-          installButtonLabel.innerText = 'Install complete :)'
+          installButtonLabel.innerText = 'Install complete :)';
+
+          ga('send', 'event', 'Inline install', 'Success');
         });
       },
       function() {
         // Inline install failed
+        ga('send', 'event', 'Inline install', 'Failed or cancelled');
+
         installButtonLabels.forEach(function(installButtonLabel) {
-          installButtonLabel.innerText = 'Redirecting...'
+          installButtonLabel.innerText = 'Redirecting...';
         });
         window.location.href = webstoreUrl;
       });
+    }
+    else {
+      ga('send', 'event', 'Inline install', 'Failed: browser was not Google Chrome.');
     }
   });
 })
